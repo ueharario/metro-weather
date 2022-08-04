c6bb072b7ce04abd791d0463c5f95dad78916b43
ueharario <uehara@backham.me>
2022-08-04 17:16:44 +0900

    commit

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <h2>新規作成</h2>
         <table border="1">
             <thead>
                 <tr>
@@ -8,9 +9,9 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>{{ userName }}</td>
-                    <td>{{ userGender }}</td>
+                <tr v-for="user in users" v-bind:key="user.id">
+                    <td>{{ user.name }}</td>
+                    <td>{{ user.gender }}</td>
                 </tr>
             </tbody>
         </table>
@@ -21,8 +22,23 @@
 export default {
     data () {
         return {
-            userName: "山田太郎",
-            userGender: "男性"
+            users: [
+                {
+                    id: 1,
+                    name: '山田太郎',
+                    gender: '男性'
+                },
+                {
+                    id: 2,
+                    name: '山田次郎',
+                    gender: '男性'
+                },
+                {
+                    id: 3,
+                    name: '山田三郎',
+                    gender: '男性'
+                }
+            ]
         }
     }
 }
