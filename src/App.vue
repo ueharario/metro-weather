c59e683f1a9aca984ad7fe0521070476749bfa2a
ueharario <uehara@backham.me>
2022-08-04 10:09:53 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <List />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import List from './components/List.vue'
 </script>
 
 <style>
---
+++ src/components/List.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="app">
+        <table id="table" border="1">
+            <thead class="thead">
+                <tr>
+                    <th>名前</th>
+                    <th>性別</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in items" v-bind:key="item.id">
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.gender }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
