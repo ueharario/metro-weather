0a3085928b8727759fc445f719c128fc77fa3927
ueharario <uehara@backham.me>
2022-08-04 10:22:44 +0900

    commit

--- src/components/List.vue
+++ src/components/List.vue
@@ -18,7 +18,28 @@
 </template>
 
 <script>
-
+const vue = new Vue({
+    el: "#app",
+    data: {
+        items: []
+    },
+    created() {
+        this.items = [
+            { id: 1,
+            name: '山田太郎',
+            gender: '男性' },
+            { id: 2,
+            name: '山田次郎',
+            gender: '男性' },
+            { id: 3,
+            name: '山田三郎',
+            gender: '男性' },
+            { id: 4,
+            name: '山田花子',
+            gender: '女性' }
+        ]
+    }
+})
 </script>
 
 <style>
