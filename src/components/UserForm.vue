4135244afa3c876ff5b6552486e76a4fb26ce15c
ueharario <uehara@backham.me>
2022-08-08 13:23:13 +0900

    UserForm.vue を少し修正しました。

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
-        <input>
-        <input>
+        <select v-model="gender">
+            <option disabled="disabled">選択してください</option>
+            <option value="male">男性</option>
+            <option value="female">女性</option>
+        </select>
         <button>やめる</button>
         <button>更新</button>
     </div>
